6eb96679a31c5320db5a533fe1c2fa20deff1c25
Vukovic Emilie <emilievukovic79@gmail.com>
2024-02-22 15:20:55 +0100

    Add RatingStar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import RatingStar from './components/RatingStar';
 
 function App() {
   return (
     <div className="App">
+      <RatingStar />
     </div>
   );
 }
---
+++ src/components/RatingStar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RatingStar() {
+  return (
+    <div>RatingStar</div>
+  )
+}
+
+export default RatingStar;
