d1dc01b13ebc675019abaa988bc08dd72704529f
Vukovic Emilie <emilievukovic79@gmail.com>
2024-02-22 17:04:43 +0100

    Try different colors for the stars : gold or yellow

--- src/components/RatingStar.tsx
+++ src/components/RatingStar.tsx
@@ -10,7 +10,7 @@ interface RatingStarProps {
 export default function RatingStar({
   maxRating = 10,
   size = 24,
-  color = '#000',
+  color = '#D4AF37', // yellow  #D4AF37 // #F8D001 // #fcc419 // #FFD700 gold
   defaultRating = 0,
 }: RatingStarProps) {
   const [rating, setRating] = useState<number>(defaultRating);
