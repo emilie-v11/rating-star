28c2f750a147eeca6bd44e4eb904c1d8be7bc0d5
Vukovic Emilie <emilievukovic79@gmail.com>
2024-02-22 15:36:55 +0100

    Create Array for stars + style container

--- src/components/RatingStar.tsx
+++ src/components/RatingStar.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
+const containerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '16px',
+};
+
 function RatingStar() {
   return (
-    <div>RatingStar</div>
-  )
+    <div style={containerStyle}>
+      <div>
+        {Array.from({ length: 10 }, (_, i) => (
+          <span
+            key={i}
+            role="img"
+            aria-label="star"
+          >
+            ⭐️
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default RatingStar;
